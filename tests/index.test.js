c935968505b73c307c2d6c6e080723eaa9f964e1
AmauryD <deflorenne.amaury@gmail.com>
2024-03-13 11:18:15 +0100

    fix linting

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,11 @@
-import { it, expect, vi } from 'vitest';
-import { getUNIXTime } from '../src';
+import { it, expect, vi } from 'vitest'
+import { getUNIXTime } from '../src'
 
 // petit test unitaire
 it('gets unix timestamp', () => {
-    // on fige le temps en 2020
-    vi.setSystemTime('2020-01-01');
-    expect(getUNIXTime()).toStrictEqual(1577836800);
-    // on restore le temps
-    vi.useRealTimers();
-});
+  // on fige le temps en 2020
+  vi.setSystemTime('2020-01-01')
+  expect(getUNIXTime()).toStrictEqual(1577836800)
+  // on restore le temps
+  vi.useRealTimers()
+})
