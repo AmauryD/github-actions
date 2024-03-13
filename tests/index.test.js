e3221f642d380d5ea4b9cdadc2fa8740f25b9153
AmauryD <deflorenne.amaury@gmail.com>
2024-03-13 13:49:49 +0100

    fix test

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,7 +4,7 @@ import { getUNIXTime } from '../src'
 // petit test unitaire
 it('gets unix timestamp', () => {
   // on fige le temps en 2020
-  vi.setSystemTime('2021-01-01')
+  vi.setSystemTime('2020-01-01')
   expect(getUNIXTime()).toStrictEqual(1577836800)
   // on restore le temps
   vi.useRealTimers()
